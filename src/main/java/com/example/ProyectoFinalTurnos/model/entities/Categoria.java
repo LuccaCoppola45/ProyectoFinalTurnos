cab579641ebaab6d5af354cd4131b2dd7c2433a2
luccacoppola <luccacoppola55@gmail.com>
2026-04-28 10:11:08 -0300

    Creacion DATABASE
    cree las entidades comercio y categoria y conecte la base da datos

--- src/main/java/com/example/ProyectoFinalTurnos/model/entities/Categoria.java
+++ src/main/java/com/example/ProyectoFinalTurnos/model/entities/Categoria.java
@@ -1,4 +1,25 @@
 package com.example.ProyectoFinalTurnos.model.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "categoria")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_categoria")
+    private long id;
+
+
+    @Column(name = "nombre", length = 50)
+    private String nombre;
 }
--- src/main/java/com/example/ProyectoFinalTurnos/model/entities/Comercio.java
+++ src/main/java/com/example/ProyectoFinalTurnos/model/entities/Comercio.java
@@ -1,4 +1,31 @@
 package com.example.ProyectoFinalTurnos.model.entities;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.antlr.v4.runtime.misc.NotNull;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
 public class Comercio {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String nombre;
+    private String direccion;
+    private int telefono;
+
+    @ManyToMany
+    @JoinTable(
+    name = "comercio_categoria", // Nombre de la tabla intermedia
+    joinColumns = @JoinColumn(name = "id_comercio"), // FK de esta clase
+    inverseJoinColumns = @JoinColumn(name = "id_categoria") // FK de la otra clase
+)
+    private List<Categoria> categorias;
 }
