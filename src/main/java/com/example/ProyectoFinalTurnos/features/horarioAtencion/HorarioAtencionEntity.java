9f1c6965e96cd9aca0ddd795c5f07aec15a0b61d
Valentin <valengonzalez2015@hotmail.com>
2026-04-28 12:07:50 -0300

    cree dentro de las clases: CancelacionEntity y HorarioAtencionEntity - Se mapeó la tabla 'cancelaciones' con relación @OneToOne hacia TurnoEntity. - Se mapeó la tabla 'horarios_atencion' con relación @ManyToOne hacia ComercioEntity. - Se implementó @Enumerated(EnumType.STRING) para guardar el nombre del día en el enum Diasemana. - Se aplicó Lombok (@Data, @NoArgsConstructor, @AllArgsConstructor) para reducir código.

--- src/main/java/com/example/ProyectoFinalTurnos/features/horarioAtencion/HorarioAtencionEntity.java
+++ src/main/java/com/example/ProyectoFinalTurnos/features/horarioAtencion/HorarioAtencionEntity.java
@@ -1,4 +1,35 @@
 package com.example.ProyectoFinalTurnos.features.horarioAtencion;
 
+import com.example.ProyectoFinalTurnos.features.comercio.ComercioEntity;
+import com.example.ProyectoFinalTurnos.model.enums.Diasemana;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "horarios_atencion")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class HorarioAtencionEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_comercio", nullable = false)
+    private ComercioEntity comercio;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "dia_semana", nullable = false)
+    private Diasemana diaSemana;
+
+    @Column(name = "hora_apertura", nullable = false)
+    private LocalTime horaApertura;
+
+    @Column(name = "hora_cierre", nullable = false)
+    private LocalTime horaCierre;
 }
